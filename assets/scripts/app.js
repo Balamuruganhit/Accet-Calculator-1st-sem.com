9a8ed35499343c2f6efb6ae8d91576d26d0386a3
Balamuruganhit <93462953+Balamuruganhit@users.noreply.github.com>
2022-08-10 02:22:42 +0100

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,69 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+
+
+// Generates and writes calculation log
+function createAndWriteOutput(calcNumber) {
+  outputResult(calcNumber); // from vendor file
+}
+
+
+
+
+function A(a){
+  if("O"==a){
+    return 10;
+  } else if("A+"==a){
+    return 9;
+  } else if("A"==a){
+    return 8;
+  } else if("B+"==a){
+    return 7;
+  } else if("B"==a){
+    return 6;
+  } else if("C"==a){
+    return 5;
+  } 
+    else if("o"==a){
+    return 10;
+  } else if("a+"==a){
+    return 9;
+  } else if("a"==a){
+    return 8;
+  } else if("b+"==a){
+    return 7;
+  } else if("b"==a){
+    return 6;
+  } else if("c"==a){
+    return 5;
+  } 
+};
+
+
+
+function divide() {
+  const enteredNumber = A(usrInput.value);
+  const enteredNumber1= A(usrInput1.value);
+  console.log(enteredNumber);
+  console.log(usrInput1);
+  console.log(enteredNumber1);
+  const enteredNumber2 = A(usrInput2.value);
+  console.log(enteredNumber2);
+  const enteredNumber3 = A(usrInput3.value);
+  console.log(enteredNumber3);
+  const enteredNumber4 = A(usrInput4.value);
+  console.log(enteredNumber4);
+  const enteredNumber5 = A(usrInput5.value);
+  console.log(enteredNumber5);
+  let sum=enteredNumber *3 +enteredNumber1 *4 +enteredNumber2 *4 +enteredNumber3 *4 +enteredNumber4 *1.5
+  +enteredNumber5 *2;
+  let avg=sum/18.5;
+  let m=avg.toFixed(2);
+  createAndWriteOutput(m);
+  
+}
+
+
+divideBtn.addEventListener('click', divide);
