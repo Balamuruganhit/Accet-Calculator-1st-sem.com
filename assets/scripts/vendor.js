26db71afb62e138defca502d0d987b7f8c0f2ac5
Balamuruganhit <93462953+Balamuruganhit@users.noreply.github.com>
2023-10-17 15:52:42 +0530

    Update vendor.js

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -4,6 +4,7 @@ const usrInput2 = document.getElementById('input-number2');
 const usrInput3 = document.getElementById('input-number3');
 const usrInput4 = document.getElementById('input-number4');
 const usrInput5= document.getElementById('input-number5');
+const usrInput6= document.getElementById('input-number6');
 const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
